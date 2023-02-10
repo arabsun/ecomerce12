7690c9ec9de331c6caf288c33f09db68b6c49d1c
Mustafa Qadoum <mostfa.qadou@gmail.com>
2023-02-10 08:48:55 -0600

    admin tab

---
+++ database/migrations/2023_02_10_144613_create_admin_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin', function (Blueprint $table) {
+            Schema::create('users', function (Blueprint $table) {
+                $table->id();
+                $table->string('first_name');
+                $table->string('last_name')->nullable();
+                $table->string('username')->unique()->nullable();
+                $table->string('photo')->nullable();
+                $table->unsignedBigInteger('role_id');
+                $table->string('email')->unique()->nullable();
+                $table->string('password');
+                $table->boolean('is_active')->default(TRUE);
+                $table->string('avatar')->nullable();
+                $table->string('slug')->nullable();
+                $table->string('phone')->unique()->nullable();
+                $table->date('date_of_birth')->nullable();
+                $table->text('description')->nullable();
+                $table->string('lang_code')->default('en');
+                $table->unsignedBigInteger('currency_id')->default(2);
+                $table->string('currency_code')->default('USD');
+                $table->rememberToken();
+                $table->timestamps();
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin');
+    }
+};
